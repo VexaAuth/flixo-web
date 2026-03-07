d8993a24a66d50e67eaa8a2edeb1a2d4f34d16d9
N の B I Շ Λ <realnobita.xyz@gmail.com>
2026-03-07 07:45:42 +0530

    Create next.config.ts

---
+++ next.config.ts
@@ -0,0 +1,14 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/api-proxy/:path*",
+        destination: `${process.env.NEXT_PUBLIC_BOT_API_URL || "http://top.visonhost.cloud:2004"}/:path*`, // Proxy to Backend
+      },
+    ];
+  },
+};
+
+export default nextConfig;
